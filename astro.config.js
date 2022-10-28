16a27e4f6b55b8dc00ab8ee53658b68b86787de0
John Joaquin Valdez <javaldez1642qc@student.fatima.edu.ph>
2022-10-28 15:57:50 +0800

    Build: Change filename of stylesheets

--- astro.config.js
+++ astro.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'astro/config'
 
-// https://astro.build/config
 export default defineConfig({
   build: {
     format: 'file',
@@ -9,7 +8,17 @@ export default defineConfig({
     build: {
       rollupOptions: {
         output: {
-          assetFileNames: 'assets/[name].[hash].[ext]',
+          assetFileNames: (info) => {
+            let extension = info.name.split('.').at(1)
+
+            if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extension)) {
+              extension = 'img'
+            } else if (/css/i.test(extension)) {
+              return `assets/[hash][extname]`
+            }
+
+            return `assets/${extension}/[name].[hash][extname]`
+          },
           chunkFileNames: 'assets/[name].[hash].js',
           entryFileNames: 'assets/[name].[hash].js',
         },

6fc23ee3a5718c44bb50e9a8636bf96353476d91
John Joaquin Valdez <javaldez1642qc@student.fatima.edu.ph>
2022-11-22 18:06:37 +0800

    Delete index.js

--- src/index.js
+++
@@ -1,6 +0,0 @@
-const mobileNavButton = document.getElementById('mobile-nav-button')
-const mobileNav = document.querySelector('.header-row-nav')
-
-mobileNavButton.addEventListener('click', () => {
-  mobileNav.classList.toggle('active')
-})

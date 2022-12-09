3867956c2d6353f3532b1ea86203877afebe8a63
John Joaquin Valdez <javaldez1642qc@student.fatima.edu.ph>
2022-12-09 15:42:34 +0800

    Add MetaIsland background

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,13 +1,16 @@
 import { Fragment } from 'react'
 import Title from '../components/Title'
 import Header from '../components/Header'
+import MetaIsland from '../backgrounds/MetaIsland'
 
 export default function Home() {
   return (
     <Fragment>
       <Title suffix="The Web 3.0 tribe for digital entrepreneurs">Meta Savages</Title>
       <Header />
-      <section className="relative min-h-[768px] overflow-hidden lg:min-h-[1024px]" />
+      <section className="relative min-h-[768px] overflow-hidden lg:min-h-[1024px]">
+        <MetaIsland />
+      </section>
     </Fragment>
   )
 }

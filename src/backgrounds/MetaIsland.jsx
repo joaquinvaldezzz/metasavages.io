69cd5a9f344268f7ace8d92af7644915214029f4
John Joaquin Valdez <javaldez1642qc@student.fatima.edu.ph>
2022-12-09 20:11:58 +0800

    Update MetaIsland.jsx

--- src/backgrounds/MetaIsland.jsx
+++ src/backgrounds/MetaIsland.jsx
@@ -220,6 +220,50 @@ export default function MetaIsland() {
           <img src="/images/bg-meta-island/artboard-10.png" alt="" />
         </div>
       </div>
+
+      <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5" role="row">
+        <div role="gridcell">
+          <img src="/images/bg-meta-island/artboard-1.png" alt="" />
+        </div>
+
+        <div role="gridcell">
+          <img src="/images/bg-meta-island/artboard-2.png" alt="" />
+        </div>
+
+        <div className="hidden md:block" role="gridcell">
+          <img src="/images/bg-meta-island/artboard-3.png" alt="" />
+        </div>
+
+        <div className="hidden md:block" role="gridcell">
+          <img src="/images/bg-meta-island/artboard-4.png" alt="" />
+        </div>
+
+        <div className="hidden lg:block" role="gridcell">
+          <img src="/images/bg-meta-island/artboard-5.png" alt="" />
+        </div>
+      </div>
+
+      <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5" role="row">
+        <div role="gridcell">
+          <img src="/images/bg-meta-island/artboard-6.png" alt="" />
+        </div>
+
+        <div role="gridcell">
+          <img src="/images/bg-meta-island/artboard-7.png" alt="" />
+        </div>
+
+        <div className="hidden md:block" role="gridcell">
+          <img src="/images/bg-meta-island/artboard-8.png" alt="" />
+        </div>
+
+        <div className="hidden md:block" role="gridcell">
+          <img src="/images/bg-meta-island/artboard-9.png" alt="" />
+        </div>
+
+        <div className="hidden lg:block" role="gridcell">
+          <img src="/images/bg-meta-island/artboard-10.png" alt="" />
+        </div>
+      </div>
     </div>
   )
 }

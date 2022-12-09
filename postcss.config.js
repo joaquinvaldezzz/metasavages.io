7242caee207b53139eec5e1013cc0976eb0bda25
John Joaquin Valdez <javaldez1642qc@student.fatima.edu.ph>
2022-12-09 14:39:46 +0800

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -2,6 +2,20 @@ const postcssSorting = require('./util/postcssSorting')
 
 module.exports = {
   plugins: {
+    '@fullhuman/postcss-purgecss': process.env.NODE_ENV === 'production' && {
+      content: [
+        './src/components/**/*.{js,jsx}',
+        './src/layouts/**/*.{js,jsx}',
+        './src/pages/**/*.{js,jsx}',
+      ],
+      defaultExtractor: (content) => {
+        const broadMatches = content.match(/[^<>"'`\s]*[^<>"'`\s:]+/g) || []
+        const innerMatches = content.match(/[^<>"'`\s.()]*[^<>"'`\s.():]+/g) || []
+        return broadMatches.concat(innerMatches)
+      },
+      keyframes: true,
+      variables: true,
+    },
     autoprefixer: {},
     'postcss-sort-media-queries': {},
     'postcss-sorting': {

db9404892f68529d6bc8413a55b596af6d14a1c1
John Joaquin Valdez <javaldez1642qc@student.fatima.edu.ph>
2022-12-09 15:39:06 +0800

    Add backgrounds folder

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ const plugin = require('tailwindcss/plugin')
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    './src/backgrounds/**/*.{js,jsx}',
     './src/components/**/*.{js,jsx}',
     './src/layouts/**/*.{js,jsx}',
     './src/pages/**/*.{js,jsx}',

e98aaffed9f65ac3d6c9af0699895249effbb433
John Joaquin Valdez <javaldez1642qc@student.fatima.edu.ph>
2022-12-09 15:12:12 +0800

    Delete unnecessary templates

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,6 @@ const plugin = require('tailwindcss/plugin')
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/backgrounds/**/*.astro',
-    './src/components/**/*.astro',
-    './src/layouts/**/*.astro',
-    './src/pages/**/*.astro',
-    './src/scripts/**/*.js',
-    // ---
     './src/components/**/*.{js,jsx}',
     './src/layouts/**/*.{js,jsx}',
     './src/pages/**/*.{js,jsx}',

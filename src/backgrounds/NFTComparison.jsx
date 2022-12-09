c801af692bb0c4be0ca7f39da69b5cc41784cc79
John Joaquin Valdez <javaldez1642qc@student.fatima.edu.ph>
2022-12-09 21:24:48 +0800

    Create backgrounds

---
+++ src/backgrounds/NFTComparison.jsx
@@ -0,0 +1,93 @@
+export default function NFTComparison() {
+  return (
+    <div className="pointer-events-none absolute select-none" role="grid">
+      <div className="grid grid-cols-3 lg:grid-cols-5" role="row">
+        <div className="hidden lg:block" role="gridcell">
+          <img src="/images/bg-roadmap/artboard-1.png" alt="" />
+        </div>
+
+        <div className="" role="gridcell">
+          <img src="/images/bg-roadmap/artboard-2.png" alt="" />
+        </div>
+
+        <div className="" role="gridcell">
+          <img src="/images/bg-roadmap/artboard-3.png" alt="" />
+        </div>
+
+        <div className="" role="gridcell">
+          <img src="/images/bg-roadmap/artboard-4.png" alt="" />
+        </div>
+
+        <div className="hidden lg:block" role="gridcell">
+          <img src="/images/bg-roadmap/artboard-5.png" alt="" />
+        </div>
+      </div>
+
+      <div className="grid grid-cols-3 lg:grid-cols-5" role="row">
+        <div className="hidden lg:block" role="gridcell">
+          <img src="/images/bg-roadmap/artboard-6.png" alt="" />
+        </div>
+
+        <div className="" role="gridcell">
+          <img src="/images/bg-roadmap/artboard-7.png" alt="" />
+        </div>
+
+        <div className="" role="gridcell">
+          <img src="/images/bg-roadmap/artboard-8.png" alt="" />
+        </div>
+
+        <div className="" role="gridcell">
+          <img src="/images/bg-roadmap/artboard-9.png" alt="" />
+        </div>
+
+        <div className="hidden lg:block" role="gridcell">
+          <img src="/images/bg-roadmap/artboard-10.png" alt="" />
+        </div>
+      </div>
+
+      <div className="grid grid-cols-3 lg:grid-cols-5" role="row">
+        <div className="hidden lg:block" role="gridcell">
+          <img src="/images/bg-roadmap/artboard-1.png" alt="" />
+        </div>
+
+        <div className="" role="gridcell">
+          <img src="/images/bg-roadmap/artboard-2.png" alt="" />
+        </div>
+
+        <div className="" role="gridcell">
+          <img src="/images/bg-roadmap/artboard-3.png" alt="" />
+        </div>
+
+        <div className="" role="gridcell">
+          <img src="/images/bg-roadmap/artboard-4.png" alt="" />
+        </div>
+
+        <div className="hidden lg:block" role="gridcell">
+          <img src="/images/bg-roadmap/artboard-5.png" alt="" />
+        </div>
+      </div>
+
+      <div className="grid grid-cols-3 lg:grid-cols-5" role="row">
+        <div className="hidden lg:block" role="gridcell">
+          <img src="/images/bg-roadmap/artboard-1.png" alt="" />
+        </div>
+
+        <div className="" role="gridcell">
+          <img src="/images/bg-roadmap/artboard-2.png" alt="" />
+        </div>
+
+        <div className="" role="gridcell">
+          <img src="/images/bg-roadmap/artboard-3.png" alt="" />
+        </div>
+
+        <div className="" role="gridcell">
+          <img src="/images/bg-roadmap/artboard-4.png" alt="" />
+        </div>
+
+        <div className="hidden lg:block" role="gridcell">
+          <img src="/images/bg-roadmap/artboard-5.png" alt="" />
+        </div>
+      </div>
+    </div>
+  )
+}
---
+++ src/backgrounds/NFTPasses.jsx
@@ -0,0 +1,93 @@
+export default function Transition2() {
+  return (
+    <div className="pointer-events-none absolute inset-0 select-none" role="grid">
+      <div className="grid grid-cols-3 lg:grid-cols-5" role="row">
+        <div className="hidden lg:block" role="gridcell">
+          <img src="/images/bg-transition-4/artboard-1.png" alt="" />
+        </div>
+
+        <div className="" role="gridcell">
+          <img src="/images/bg-transition-4/artboard-2.png" alt="" />
+        </div>
+
+        <div className="" role="gridcell">
+          <img src="/images/bg-transition-4/artboard-3.png" alt="" />
+        </div>
+
+        <div className="" role="gridcell">
+          <img src="/images/bg-transition-4/artboard-4.png" alt="" />
+        </div>
+
+        <div className="hidden lg:block" role="gridcell">
+          <img src="/images/bg-transition-4/artboard-5.png" alt="" />
+        </div>
+      </div>
+
+      <div className="grid grid-cols-3 lg:grid-cols-5" role="row">
+        <div className="hidden lg:block" role="gridcell">
+          <img src="/images/bg-transition-4/artboard-1.png" alt="" />
+        </div>
+
+        <div className="" role="gridcell">
+          <img src="/images/bg-transition-4/artboard-2.png" alt="" />
+        </div>
+
+        <div className="" role="gridcell">
+          <img src="/images/bg-transition-4/artboard-3.png" alt="" />
+        </div>
+
+        <div className="" role="gridcell">
+          <img src="/images/bg-transition-4/artboard-4.png" alt="" />
+        </div>
+
+        <div className="hidden lg:block" role="gridcell">
+          <img src="/images/bg-transition-4/artboard-5.png" alt="" />
+        </div>
+      </div>
+
+      <div className="grid grid-cols-3 lg:grid-cols-5" role="row">
+        <div className="hidden lg:block" role="gridcell">
+          <img src="/images/bg-transition-4/artboard-1.png" alt="" />
+        </div>
+
+        <div className="" role="gridcell">
+          <img src="/images/bg-transition-4/artboard-2.png" alt="" />
+        </div>
+
+        <div className="" role="gridcell">
+          <img src="/images/bg-transition-4/artboard-3.png" alt="" />
+        </div>
+
+        <div className="" role="gridcell">
+          <img src="/images/bg-transition-4/artboard-4.png" alt="" />
+        </div>
+
+        <div className="hidden lg:block" role="gridcell">
+          <img src="/images/bg-transition-4/artboard-5.png" alt="" />
+        </div>
+      </div>
+
+      <div className="grid grid-cols-3 lg:grid-cols-5" role="row">
+        <div className="hidden lg:block" role="gridcell">
+          <img src="/images/bg-transition-4/artboard-1.png" alt="" />
+        </div>
+
+        <div className="" role="gridcell">
+          <img src="/images/bg-transition-4/artboard-2.png" alt="" />
+        </div>
+
+        <div className="" role="gridcell">
+          <img src="/images/bg-transition-4/artboard-3.png" alt="" />
+        </div>
+
+        <div className="" role="gridcell">
+          <img src="/images/bg-transition-4/artboard-4.png" alt="" />
+        </div>
+
+        <div className="hidden lg:block" role="gridcell">
+          <img src="/images/bg-transition-4/artboard-5.png" alt="" />
+        </div>
+      </div>
+    </div>
+  )
+}

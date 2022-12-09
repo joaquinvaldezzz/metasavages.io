5c4cf0289e8c95b0bec46c1c7fdb078c36cdd82f
John Joaquin Valdez <javaldez1642qc@student.fatima.edu.ph>
2022-12-09 15:24:35 +0800

    Import Header component

---
+++ src/pages/index.jsx
@@ -0,0 +1,13 @@
+import { Fragment } from 'react'
+import Title from '../components/Title'
+import Header from '../components/Header'
+
+export default function Home() {
+  return (
+    <Fragment>
+      <Title suffix="The Web 3.0 tribe for digital entrepreneurs">Meta Savages</Title>
+
+      <Header />
+    </Fragment>
+  )
+}
